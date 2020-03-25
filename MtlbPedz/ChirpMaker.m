443349f9d63dc52499b8ebc74782f4df95d64a5e
Alec Wright <alecpwright@gmail.com>
2020-03-25 07:42:39 +0200

    Developing LFO extraction technique, still quite messy now.

--- MtlbPedz/ChirpMaker.m
+++ MtlbPedz/ChirpMaker.m
@@ -1,6 +1,6 @@
-function x = ChirpMaker(chirps, spacing, M, a, fs) 
+function [x, chrpLen] = ChirpMaker(chirps, spacing, M, a) 
 
-    sps = round(fs*spacing/1000);
+    
  
     delhf = (1 - a^2)/(1 + 2*a*cos(pi) + a^2);
     dellf = (1 - a^2)/(1 + 2*a*cos(0) + a^2);
@@ -8,10 +8,12 @@ function x = ChirpMaker(chirps, spacing, M, a, fs)
     
     x = 0;
     for ch = 0:chirps-1
-       x(ch*sps + 1) = 1; 
+       x(ch*spacing + 1) = 1; 
     end
     
-    x(end+1:end+(round(M*grpdel)) + 50) = 0;
+    chrpLen = round(M*grpdel);
+    
+    x(end+1:end+(chrpLen + 50)) = 0;
     
     for n = 1:M
         x = filter([a, 1], [1, a], x);
