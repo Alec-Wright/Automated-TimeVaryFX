89d243424d26c218d043e5c0fe939e48c9e0e201
Alec Wright <alecpwright@gmail.com>
2020-11-16 10:13:47 +0200

    altered NewMasterScript to iterate through possible measurement settings

--- LFOMeasurement/NewMasterScript.m
+++ LFOMeasurement/NewMasterScript.m
@@ -7,33 +7,69 @@ addpath('../DigitalEffectsPedals')
 
 PedalName = 'PhaserSweetTone';
 Digi = 1;
-Rate = 0.3;
-SNR = 40;
+Rate = [0.3, 0.6];
+SNR = [20,40];
+
+ch_ty = [1,2];
+
+ch_len = [8,18,28,38];
+ch_spc = [10,20,30,40];
 
 fs = 44100;
 
+for r = 1:length(Rate)
+    ra = Rate(r);
+    Signals(r) = SignalHolder();
+    Signals(r) = Signals(r).SigGen(1, 28, 30, 20, 22050, 10, fs);
+    ProcSigs(r) = SignalProcessor(PedalName, Digi, ra);
+    ProcSigs(r) = ProcSigs(r).SigProc(Signals(r).SigGet(1), ra, 20, fs);
+     
+    AnlySig(r) = SignalAnalyser(Signals.Signals, ProcSigs.ProcessedSignals);
+    AnlySig(r) = AnlySig(r).SpecExtract(0.5, 1);
+    AnlySig(r) = AnlySig(r).PrelimAnly(1);
+    
+    st_f = 0.9*AnlySig.Min_f;
+    en_f = 1.2*AnlySig.Max_f;
+    
+    i = 2;
+    
+    for snr = 1:length(SNR)
+        for ty = 1:length(ch_ty)
+            for ch_le = 1:length(ch_len)
+                
+                type = ch_ty(ty);
+                sn = SNR(snr);
+                
+                ch_l = ch_len(ch_le);
+                ch_s = ch_spc(ch_le);
+                
+                Signals(r) = Signals(r).SigGen(type, ch_l, ch_s, st_f, en_f, 10, fs);
+                
+                ProcSigs(r) = ProcSigs(r).SigProc(Signals.SigGet(i), ra, sn, fs);
+                AnlySig(r) = AnlySig(r).Update(Signals(r).Signals, ProcSigs(r).ProcessedSignals);
+                AnlySig(r) = AnlySig(r).SpecExtract(0.5, i);
+                AnlySig(r) = AnlySig(r).LFOTrack(i, 5);
+                
+                lfo =  AnlySig(r).Measured_LFOs{i-1, 'Measured_LFO'};
+                tAx =  AnlySig(r).Measured_LFOs{i-1, 'LFO_time_axis'};
+                plot(tAx{1,1}, lfo{1,1});
+                    
+                i = i + 1;
+            end
+        end
+    end
+end
 
-Signals = SignalHolder();
-Signals = Signals.SigGen(1, 28, 30, 20, 22050, 10, fs);
 
 
-ProcSigs = SignalProcessor(PedalName, Digi, Rate);
-ProcSigs = ProcSigs.SigProc(Signals.SigGet(1), Rate, SNR, fs);
 
 
-AnlySig = SignalAnalyser(Signals.Signals, ProcSigs.ProcessedSignals);
-AnlySig = AnlySig.SpecExtract(0.5, 1);
 
 
-AnlySig = AnlySig.PrelimAnly(1);
 
-st_f = 0.9*AnlySig.Min_f;
-en_f = 1.2*AnlySig.Max_f;
-Signals = Signals.SigGen(1, 28, 30, st_f, en_f, 10, fs);
-ProcSigs = ProcSigs.SigProc(Signals.SigGet(2), Rate, SNR, fs);
-AnlySig = AnlySig.Update(Signals.Signals, ProcSigs.ProcessedSignals);
-AnlySig = AnlySig.SpecExtract(0.5, 2);
 
-AnlySig = AnlySig.LFOTrack(2, 5);
+
+
+
 
 
